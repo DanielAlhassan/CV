3bec9f1d795cbbad24bede8ea49a96e28151a625
Daniel Alhassan <danhmed@gmail.com>
2022-06-16 02:26:16 -0500

    fixed tribble error

--- r/data.R
+++ r/data.R
@@ -1,32 +1,39 @@
-# projects <- tribble(
-#   ~area, ~accomplishment, ~year, ~where, ~detail,
-#   "Lean Six Sigma Green Belt --- Reception Center Intake Process", "California Correctional Health Care Services", 2019, "Region 1 Quality Management", "Completed a Lean Six Sigma Green Belt project on the reception center medical screening process at San Quentin State Prison improving compliance from 2% and a baseline capability aggregate of 29.96% compliance to 84% during the transition phase of the project through application of the lean 6 methodologies and tools. Scope of project affects 6 total state prisons with 3 more to be positively impacted by state wide changes resulting from this project",
-#   "Universal Data Scrubber", "California Correctional Health Care Services", 2018, "Region 1 Quality Management", "Creation of a Universal Data Scrubbing tool sent statewide to take most department SQL Report Server exports and turn them into raw data sets for Excel Analytics for end user staff"
-# )
+research.interests <- tribble(
+  ~area, ~subarea,
+  "Statistical Genomics |", "Statistical methods for detecting differential DNA methylation region",
+  "Survival Analysis |", "Modeling event history data with a cure fraction",
+  "Inter-disciplinary Research |", "statistical applications in health, engineering and social science",
+  "Statistical Machine Learning |", "Methods that combine classical statistical with machine learning"
+  )
 
 certifications <- tribble(
   ~area, ~accomplishment, ~year, ~where, ~detail,
-  "IBM & Coursera", "IBM Data Science Professional Certificate", "2019", "Online", NA,
   "American Statistical Association", "Graduate Statistician", "2020", "ASA", NA,
+  "IBM & Coursera", "IBM Data Science Professional Certificate", "2019", "Online", NA,
   "DataCamp", "Data Scientist with R", "2019", "Online", NA,
 )
 
 skills <- tribble(
   ~area, ~skills,
-  "Programming Languages", "Python, R, LaTeX, Markdown, RMarkdown, Git, RStudio, Excel, Outlook, OneNote, PowerPoint, Word ",
+  "Programming Languages & Other Tools", "Python, R, LaTeX, Markdown, RMarkdown, Git, RStudio, Excel, Outlook, OneNote, PowerPoint, Word ",
 )
 
 honors <- tribble(
   ~area, ~accomplishment, ~year, ~where, ~detail,
   "Midwestern Association of Graduate Schools (MAGS)", "Excellence in Teaching Award Nominee", 2021, " Missouri S&T","Each department nominates one graduate student for this award",
+  "Department of Mathematics & Statistics", "GTA Teaching Excellence Award" , 2020, " Missouri S&T", NA,
+  "Department of Mathematics & Statistics", "GTA Teaching Excellence Award" , 2018, " Missouri S&T", NA,
+  "Department of Mathematics", "Graduate Mathematics Faculty Award", 2017 , "University of New Orleans", NA
 )
 
 edu <- tribble(
   ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
-  "Missouri University of Science and Technology", 2017, NA, "Ph.D. Mathematics (Statistics Emphasis)", "Rolla, MO ","Advisors: Dr. Akim Adekpedjou & Dr. Gayla Olbricht",
-  "University of New Orleans", 2015, 2017, "M.S. Mathematics ", "New Orleans",NA,
+  "Missouri University of Science and Technology", 2017, NA, "Ph.D. Mathematics (Statistics Emphasis)", "Rolla, MO", "Advisors: Dr. Akim Adekpedjou & Dr. Gayla Olbricht",
+  "Missouri University of Science and Technology", 2017, NA, "Ph.D. Mathematics (Statistics Emphasis)", "Rolla, MO", "GPA: 3.93",
+  "University of New Orleans", 2015, 2017, "M.S. Mathematics ", "New Orleans", "GPA: 3.9",
   "Kwame Nkrumah University of Science and Technology", 2009, 2013, "B.Sc. Actuarial Science", "Kumasi Ghana", "Thesis: Estimation of Beta and the Relationship Between Share Return and Beta in the Emerging
-Market of Ghana. (coauthored) \n Advisor: Dr. Lord Mensah",
+Market of Ghana. (coauthored) ",
+  "Kwame Nkrumah University of Science and Technology", 2009, 2013, "B.Sc. Actuarial Science", "Kumasi Ghana", "Advisor: Dr. Lord Mensah"
 )
 
 work <- tribble(
@@ -34,11 +41,28 @@ work <- tribble(
   "Statistical Consultant", "Missouri S&T ASA Student Chapter", "August", 2018, "Present", NA, "Rolla - MO", "Provide statistical data analysis guidelines to student community",
   "Statistical Consultant", "Missouri S&T ASA Student Chapter", "August", 2018, "Present", NA, "Rolla - MO",  "Assist students with with R, JMP, Minitab and SAS",
   "Statistical Consultant", "Missouri S&T ASA Student Chapter", "August", 2018, "Present", NA, "Rolla - MO", "Help students with experimental design issues at the planning stages of project work",
-  "Graduate Teaching Assistant", "Missouri University of Science and Technology", "August", 2018, "Present", NA, "Rolla - MO", "TOOLS USED --- Git, VS Code, Visual Studio, Azure DevOps, SQL Server Management Studio",
+  "Instructor", "Missouri University of Science and Technology", "August", 2018, "Present", NA, "Rolla - MO", "Applied Engineering Statistics (multiple offerings)--- Textbook: Applied Statistics and Probability for Engineers by Douglas Montgomery et al",
+  "Instructor", "Missouri University of Science and Technology", NA, 2021, NA, NA, "Rolla - MO", "Introduction to Biostatistics --- Textbook: Statistics for the Life Sciences by Samuels and Witmer",
+  "Graduate Teaching Assistant", "Missouri University of Science and Technology", "August", 2017, "May", 2018, "Rolla - MO", "Calculus I & II",
+  "Graduate Teaching Assistant", "University of New Orleans", "August", 2015, "May", 2016, "New Orleans - LA", "Calculus I, II & III",
+  "Instructor", "University of New Orleans", "August", 2016, "May", 2017, "New Orleans - LA", "Elementary Statistics & Statistics for Business and Economics",
+
 )
 
-works <- tribble(
+manuinprep <- tribble(
   ~area, ~accomplishment, ~year, ~where, ~detail,
-  "Runes", "Convert a string of text characters to Elder Futhark Runes ", "February 2020", "https://github.com/tallguyjenks/runes", "Author and Maintainer",
-  "Runes", "Convert a string of text characters to Elder Futhark Runes ", "February 2020", "https://github.com/tallguyjenks/runes", "Released to CRAN",
+  "Manuscripts in Preparation", "A Normalized Kernel-weighted Approach to Identifying Differential Methylation Regions in the Human Genome", 2022, NA, "(with G. Olbricht and A. Adekpedjou)",
+  "Manuscripts in Preparation", "Differential Methylation Region Detection via an Array-adaptive Normalized Kernel-weighted Model", 2022, NA, "(with G. Olbricht and A. Adekpedjou)",
+  )
+
+conference <- tribble(
+  ~area, ~accomplishment, ~year, ~where, ~detail,
+  "Innovation in Teaching and Learning Conference", "", "March, 2022", "Missouri S & T", NA,
+  "Conference on Statistical Practice", "","February, 2019 ", "New Orleans LA", NA
 )
+
+talks <- tribble(
+  ~area, ~accomplishment, ~year, ~where, ~detail,
+  "Talk", "Differential Methylation Region Detection via an Array-adaptive Normalized Kernel-weighted Model", "April 2022", "Graduate Seminar at Missouri S&T", NA,
+  "Workshop", "LATEX Workshop Sessions I & II", "2018", "Missouri S & T ASA Student Chapter", "Led a two-day workshop teaching graduate students how to typset with Latex",
+  )
